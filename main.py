f00fc1bb1d6d78b0aa07faad8868f2615a9f3549
murphypicard <ayaghsizian@gmail.com>
2017-01-19 20:13:44 -0500

    movie of the day

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import webapp2
+from random import randint
 
 class Index(webapp2.RequestHandler):
 
     def getRandomMovie(self):
 
-        # TODO: make a list with at least 5 movie titles
-
-        # TODO: randomly choose one of the movies, and return it
-
-        return "The Big Lebowski"
+        # # TODO: make a list with at least 5 movie titles
+        movies = ["Edge of Tomorrow", "Home Alone", "Bad Santa", "Finding Nemo", "Rat Race"]
+        #
+        # # TODO: randomly choose one of the movies, and return it
+        # movies.random
+        return movies[randint(0,4)]
 
     def get(self):
         # choose a movie by invoking our new function
@@ -16,6 +18,9 @@ class Index(webapp2.RequestHandler):
 
         # build the response string
         content = "<h1>Movie of the Day</h1>"
+        content += "<p>" + movie + "</p>"
+        content += "<h1>The Movie Tomorrw</h1>"
+        movie = self.getRandomMovie()
         content += "<p>" + movie + "</p>"
 
         # TODO: pick a different random movie, and display it under
